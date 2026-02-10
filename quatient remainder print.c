b3eb9c9f34f8fb493d61523cdd3dba76dfd16ac5
e24ai073 <e24ai073@shanmugha.edu.in>
2026-02-10 11:41:08 +0530

    Add files via upload

---
+++ quatient remainder print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,quotient,remainder;
+    scanf("%d""%d",&a,&b);
+    quotient=a/b;
+    remainder=a%b;
+    printf("quotient:");
+    printf("%d ",quotient);
+    printf("remainder:");
+    printf("%d",remainder);
+    
+    
+    return 0;
+}
