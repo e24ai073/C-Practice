db619ee85a38201beb81b9be2f600bc614e72ac2
e24ai073 <e24ai073@shanmugha.edu.in>
2026-02-12 11:42:41 +0530

    Add files via upload

---
+++ check 3 digit number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num;
+    scanf("%d" ,&num);
+    if(num>=100&&num<=999){
+        
+    printf("three digit number");
+    }
+    else{
+        printf("not three digit number");
+    }
+    return 0;
+}
