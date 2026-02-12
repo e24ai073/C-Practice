0745fd8ad87b0b0c17151009f2d4332dc3adc856
e24ai073 <e24ai073@shanmugha.edu.in>
2026-02-12 11:10:24 +0530

    Add files via upload

---
+++ multiple of 3 and 7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num;
+    scanf("%d",&num);
+    if(num% 3==0&&num%7==0){
+        
+    printf("multiply by 3 and 7");
+    }
+    else{
+        printf("not multiply by 3 and 7");
+    }
+    return 0;
+}
