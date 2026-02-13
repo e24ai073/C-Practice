676de3a95979919041a90336d58bc173f98a30d0
e24ai073 <e24ai073@shanmugha.edu.in>
2026-02-13 09:49:44 +0530

    Add files via upload

---
+++ smallest using ternary oprator.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    (num1>num2) ? printf("%d is smallest",num2) : printf("%d is smallest",num1);
+    return 0;
+    
+}
