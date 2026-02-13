4484bf546173526e2eed7cfb51d196c34bc1bf40
e24ai073 <e24ai073@shanmugha.edu.in>
2026-02-13 11:12:31 +0530

    Add files via upload

---
+++ simple calculator(switch).c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    int a, b, result;
+    char op;
+    scanf("%d%c%d", &a, &op, &b);
+    switch(op) {
+    case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+        case '%':
+            result = a % b;
+            break;
+        default:
+            printf("Invalid operator");
+            return 0;
+    }
+    printf("result=%d", result);
+
+    return 0;
+}
