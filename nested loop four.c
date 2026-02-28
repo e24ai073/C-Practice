84c3b5ba3a7a631d3f84c7dcf10405f28040afa5
e24ai073 <e24ai073@shanmugha.edu.in>
2026-02-28 11:55:40 +0530

    Add files via upload

---
+++ nested loop four.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int i=1,j;
+    char ch='A';
+    while(i <= 5){
+        j = 1;
+        while(j <= i){
+            printf("%c", ch);
+            
+            j++;
+        }
+    printf("\n");
+    ch++;
+    i++;
+    }
+    return 0;
+}
