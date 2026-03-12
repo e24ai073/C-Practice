e75a81d7cd35749c4108c3a96c44d9cb71f24fe0
e24ai073 <e24ai073@shanmugha.edu.in>
2026-03-12 16:12:29 +0530

    Add files via upload

---
+++ string lowercase uppercase convertor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    char str[200];
+    int i=0;
+    scanf("%[^\n]",str);  
+    while(str[i] !='\0'){
+        if(str[i]>='a'&&str[i]<='z'){
+            str[i]=str[i]-32; 
+        }
+        printf("%c",str[i]);
+        i++;
+    }
+    return 0;
+}
