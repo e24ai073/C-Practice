8371b9e698925fe276f677fa2ca9486c1952132a
e24ai073 <e24ai073@shanmugha.edu.in>
2026-03-12 15:49:33 +0530

    Add files via upload

---
+++ string consonent counter.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    char str[200];
+    int i=0,count=0;
+    scanf("%[^\n]",str);   
+    while(str[i] !='\0'){
+        if((str[i]>='a'&&str[i]<='z')||(str[i]>='A'&&str[i]<='Z')){
+            if (!(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||
+                  str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')){
+                count++;
+            }
+        }
+        i++;
+    }
+    printf("%d",count);
+    return 0;
+}
