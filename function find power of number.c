02a012daf490dfe56366f89af44cc1c54d04b7d6
e24ai073 <e24ai073@shanmugha.edu.in>
2026-03-17 13:45:16 +0530

    Add files via upload

---
+++ function find power of number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int power(int base, int exponent){
+    int result=1;
+    for (int i=1;i<=exponent;i++){
+        result=result*base;
+    }
+    return result;
+}
+int main() {
+    int base,exponent;
+    printf("Enter base: ");
+    scanf("%d",&base);
+    printf("Enter exponent: ");
+    scanf("%d",&exponent);
+    int result = power(base,exponent);
+    printf("Result: %d\n",result);
+    return 0;
+}
